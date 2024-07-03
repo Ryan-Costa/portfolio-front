b381eff0c8a6dd4562c3cabc24056b357638e446
Ryan <ryansouzadev@gmail.com>
2024-07-03 00:39:10 -0300

    feat: first commit (create project)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "var(--color-primary)",
+        text: "var(--color-text)",
+        background: "var(--color-background)",
+      },
+      transitionDuration: {
+        "2000": "2000ms", // Adiciona uma duração de transição de 2 segundos
       },
     },
   },
